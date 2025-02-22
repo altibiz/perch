fd577c2fea24b2918332dffe4470a6d183e8f2ea
Hrvoje Jurić <hrvoje@altibiz.com>
2025-02-22 08:35:33 +0100

    WIP

--- src/flake/defaults.nix
+++ src/flake/defaults.nix
@@ -1,23 +1,19 @@
-{ self, lib, ... }:
+{ self, lib, config, ... }:
 
 {
   options.seal.defaults.nixpkgs.overlays = lib.mkOption {
-    # FIXME: type error
-    # type = lib.types.listOf self.lib.type.overlay;
-    type = lib.types.raw;
-    default = [
-      # FIXME: default not found because evaluation context
-      # config.flake.overlays.default 
-    ];
+    type = lib.types.listOf self.lib.type.overlay;
+    default =
+      if config.flake.overlays ? default
+      then [ config.flake.overlays.default ]
+      else [ ];
     description = lib.literalMD ''
       Nixpkgs config used for defaults in flake outputs.
     '';
   };
 
   options.seal.defaults.nixpkgs.config = lib.mkOption {
-    # FIXME: type error
-    # type = self.lib.type.nixpkgs.config;
-    type = lib.types.raw;
+    type = self.lib.type.nixpkgs.config;
     default = { };
     description = lib.literalMD ''
       Nixpkgs config used for defaults in flake outputs.
