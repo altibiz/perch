b0f5048f91b95eb5e1d7f7eda6b40058443e43f5
Hrvoje Jurić <hrvoje@altibiz.com>
2025-12-17 11:28:07 +0100

    wip

---
+++ test/unit/specs/factory.nix
@@ -0,0 +1,66 @@
+{ self, nixpkgs, ... }:
+
+let
+  submoduleResult = { specialArgs, flakeModules, ... }:
+    self.lib.factory.submoduleModule {
+      inherit specialArgs flakeModules;
+      config = "nixosModule";
+    };
+
+  artifactResult = { specialArgs, nixpkgs, flakeModules, ... }:
+    self.lib.factory.artifactModule {
+      inherit specialArgs nixpkgs flakeModules;
+      config = "package";
+      nixpkgsConfig = "packageNixpkgs";
+    };
+
+  flakeResult = self.lib.flake.make {
+    inputs = {
+      inherit nixpkgs;
+      input = {
+        modules.default = {
+          imports = [
+            submoduleResult
+            artifactResult
+          ];
+        };
+      };
+    };
+    selfModules = {
+      x86_64_Only = {
+        nixosModule = { value = "x86_64 hello :)"; };
+        package = "x86_64 hello :)";
+        packageNixpkgs.system = "x86_64-linux";
+      };
+      allDefaultSystems = {
+        nixosModule = { value = "hello all default systems :)"; };
+        package = "hello all default systems :)";
+      };
+    };
+  };
+in
+rec {
+  factory_submodule_artifact_correct = flakeResult == {
+    packages = {
+      "aarch64-darwin" = {
+        allDefaultSystems = "hello all default systems :)";
+      };
+      "aarch64-linux" = {
+        allDefaultSystems = "hello all default systems :)";
+      };
+      "x86_64-darwin" = {
+        allDefaultSystems = "hello all default systems :)";
+      };
+      "x86_64-linux" = {
+        allDefaultSystems = "hello all default systems :)";
+        x86_64_Only = "x86_64 hello :)";
+      };
+    };
+    nixosModules = {
+      allDefaultSystems = { value = "hello all default systems :)"; };
+      x86_64_Only = { value = "x86_64 hello :)"; };
+    };
+  };
+  factory_submodule_correct = factory_submodule_artifact_correct;
+  factory_artifact_correct = factory_submodule_artifact_correct;
+}
